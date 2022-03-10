6354a2eb6d80be78b84b64cdfecc36449ba6e1c4
unknown <Sathishkumar A>
2022-03-11 01:20:13 +0530

    changs

--- lib/ec2-cdk-stack.ts
+++ lib/ec2-cdk-stack.ts
@@ -12,11 +12,11 @@ export class Ec2CdkStack extends cdk.Stack {
 
     // Create a Key Pair to be used with this EC2 Instance
     // Temporarily disabled since `cdk-ec2-key-pair` is not yet CDK v2 compatible
-     const key = new KeyPair(this, 'KeyPair', {
+    const key = new KeyPair(this, 'KeyPair', {
       name: 'cdk-keypair',
-       description: 'Key Pair created with CDK Deployment',
-     });
-     key.grantReadOnPublicKey
+    //   description: 'Key Pair created with CDK Deployment',
+    });
+    key.grantReadOnPublicKey
 
     // Create new VPC with 2 Subnets
     const vpc = new ec2.Vpc(this, 'VPC', {
@@ -54,7 +54,7 @@ export class Ec2CdkStack extends cdk.Stack {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
       machineImage: ami,
       securityGroup: securityGroup,
-       keyName: key.keyPairName,
+      keyName: key.keyPairName,
       role: role
     });
 
--- src/config.sh
+++ src/config.sh
@@ -3,4 +3,5 @@
 # Update with optional user data that will run on instance start.
 # Learn more about user-data: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
 
-mkdir test
+mkdir -p /home/ec2-user/test
+touch /home/ec2-user/file
