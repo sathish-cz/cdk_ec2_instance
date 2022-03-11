c5bb140ae3b85b65c0260b4e215c2782bed7928a
unknown <Sathishkumar A>
2022-03-11 09:42:44 +0530

    shell file to LF

--- src/config.sh
+++ src/config.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -xe
-
 # Update with optional user data that will run on instance start.
 # Learn more about user-data: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
-
 mkdir -p /home/ec2-user/test
 touch /home/ec2-user/file
